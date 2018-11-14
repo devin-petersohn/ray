03dc176653621c6418cc2dba2400128eff8b3299
Devin Petersohn <devin.petersohn@gmail.com>
2018-11-14 14:47:51 -0800

    Test

--- python/ray/test/test_modin.py
+++ python/ray/test/test_modin.py
@@ -18,8 +18,10 @@ import ray  # noqa F401
     sys.version_info[0] < 3, reason="Modin is not imported in Python2")
 def test_modin_import():
     # The import must be here because python2 is not supported yet
-    raise ValueError(sys.path)
-    import modin.pandas as pd
+    try:
+        import modin.pandas as pd
+    except ImportError:
+        raise ValueError(sys.path)
     frame_data = [1, 2, 3, 4, 5, 6, 7, 8]
     frame = pd.DataFrame(frame_data)
     assert frame.sum().squeeze() == sum(frame_data)
